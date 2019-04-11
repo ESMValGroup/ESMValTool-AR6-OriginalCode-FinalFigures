ce6f8495f3218fe95f0ca8aa894a8ef630cd57ed
Lee de Mora <ledm@pml.ac.uk>
2019-04-11 11:34:00 +0100

    Added extra models to the recipe, added observational data to diagnostic, and made several other changes.

--- esmvaltool/diag_scripts/ocean/diagnostic_fig_3_21.py
+++ esmvaltool/diag_scripts/ocean/diagnostic_fig_3_21.py
@@ -1,39 +1,6 @@
 """
-Model vs Observations maps Diagnostic.
+Figure 3.21
 ======================================
-
-Diagnostic to produce comparison of model and data.
-The first kind of image shows four maps and the other shows a scatter plot.
-
-The four pane image is a latitude vs longitude figures showing:
-
-* Top left: model
-* Top right: observations
-* Bottom left: model minus observations
-* Bottom right: model over observations
-
-
-The scatter plots plot the matched model coordinate on the x axis, and the
-observational dataset on the y coordinate, then performs a linear
-regression of those data and plots the line of best fit on the plot.
-The parameters of the fit are also shown on the figure.
-
-Note that this diagnostic assumes that the preprocessors do the bulk of the
-hard work, and that the cube received by this diagnostic (via the settings.yml
-and metadata.yml files) has no time component, a small number of depth layers,
-and a latitude and longitude coordinates.
-
-An approproate preprocessor for a 3D + time field would be::
-
-  preprocessors:
-    prep_map:
-      extract_levels:
-        levels:  [100., ]
-        scheme: linear_extrap
-      time_average:
-      regrid:
-        target_grid: 1x1
-        scheme: linear
 
 This tool is part of the ocean diagnostic tools package in the ESMValTool,
 and was based on the plots produced by the Ocean Assess/Marine Assess toolkit.
@@ -55,6 +22,7 @@ import iris.quickplot as qplt
 import numpy as np
 from scipy.stats import linregress
 import cartopy
+import cartopy.crs as ccrs
 
 from esmvaltool.diag_scripts.ocean import diagnostic_tools as diagtools
 from esmvaltool.diag_scripts.shared import run_diagnostic
@@ -84,6 +52,7 @@ def add_map_subplot(subplot, cube, nspace, title='', cmap='', log=False, cbar=Tr
         Flag to plot the colour scale linearly (False) or
         logarithmically (True)
     """
+    assert 0
     plt.subplot(subplot)
     logger.info('add_map_subplot: %s', subplot)
     if log:
@@ -117,6 +86,113 @@ def add_map_subplot(subplot, cube, nspace, title='', cmap='', log=False, cbar=Tr
     #plt.title(title)
 
 
+def load_cube_221():
+    # the 1950-2000 observational change.
+    path = "/data/sthenno1/scratch/ledm/Observations/CSIRO/"
+    path += "DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc"
+    # Downloaded from https://www.cmar.csiro.au/cgi-bin/oceanchange?file=DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc
+    # 2019-04-08
+    cubes = iris.load(path)
+    cube_out = ''
+    for cube in cubes:
+        if cube.standard_name == 'change_over_time_in_sea_water_practical_salinity':
+            cube_out = cube
+    cube_out = cube_out.collapsed('time', iris.analysis.MEAN)
+    cube_out = cube_out.extract(iris.Constraint(sea_water_pressure=0.))
+    return cube_out
+
+
+def load_cube_222():
+    # the 1950-2000 observational climatolocal mean.
+    path = "/data/sthenno1/scratch/ledm/Observations/CSIRO/"
+    path += "DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc"
+    # Downloaded from https://www.cmar.csiro.au/cgi-bin/oceanchange?file=DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc
+    # 2019-04-08
+    cubes = iris.load(path)
+    cube_out = ''
+    for cube in cubes:
+        if cube.standard_name == 'sea_water_practical_salinity':
+            cube_out = cube
+    cube_out = cube_out.collapsed('time', iris.analysis.MEAN)
+    cube_out = cube_out.extract(iris.Constraint(sea_water_pressure=0.))
+    return cube_out
+
+def load_cube_trends():
+    # the 1950-2000 observational climatolocal mean.
+    path = "/data/sthenno1/scratch/ledm/Observations/CSIRO/"
+    path += "DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc"
+    # Downloaded from https://www.cmar.csiro.au/cgi-bin/oceanchange?file=DurackandWijffels_GlobalOceanSurfaceChanges_1950-2000.nc
+    # 2019-04-08
+
+    cubes = iris.load(path)
+    cube_out = ''
+    for cube in cubes:
+        if cube.long_name == 'Salinity change error 1950-2000':
+            cube_out = cube
+    cube_out = cube_out.collapsed('time', iris.analysis.MEAN)
+    cube_out = cube_out.extract(iris.Constraint(sea_water_pressure=0.))
+    return cube_out
+
+def add_coasts():
+    plt.gca().add_feature(cartopy.feature.LAND, facecolor=[0.8, 0.8, 0.8])
+    plt.gca().coastlines()
+
+
+def make_mean_of_cube_list(cube_list):
+    """
+    Takes the mean of a list of cubes (not an iris.cube.CubeList).
+
+    Assumes all the cubes are the same shape.
+    """
+    cube_mean = cube_list[0]
+    for cube in cube_list[1:]:
+        cube_mean+=cube
+    cube_mean = cube_mean/ float(len(cube_list))
+    cube_mean.units = '1.0'
+    return cube_mean
+
+
+def prepare_model_metadata(cfg):
+    metadatas = diagtools.get_input_files(cfg)
+    metadata_files = metadatas.keys()
+    model_metadatas = {}
+    model_cubes = {}
+
+    ##########################
+    # make list of filenames for each years.
+    for fn in metadata_files:
+        print('\nmetadata_file:', fn)
+        print(metadatas[fn])
+        for year in [1950, 2000, 2050, 2099]:
+            if metadatas[fn]['start_year'] == metadatas[fn]['end_year'] == year:
+                try:
+                    model_metadatas[str(year)].append(fn)
+                except:
+                    model_metadatas[str(year)] = [fn,]
+
+        if metadatas[fn]['start_year'] == 1950 and metadatas[fn]['end_year'] == 2000:
+            try:
+                model_metadatas['1950-2000'].append(fn)
+            except:
+                model_metadatas['1950-2000'] = [fn,]
+
+        if metadatas[fn]['start_year'] == 2050 and metadatas[fn]['end_year'] == 2099:
+            try:
+                model_metadatas['2050-2099'].append(fn)
+            except:
+                model_metadatas['2050-2099'] = [fn,]
+
+    ##########################
+    # convert list of fn to multi model mean.
+    for name, model_fns in model_metadatas.items():
+        cubes = []
+        for model_fn in model_fns:
+            cube = iris.load_cube(model_fn)
+            cubes.append(cube)
+        model_cubes[name] = make_mean_of_cube_list(cubes)
+
+    return model_cubes
+
 
 def main(cfg):
     """
@@ -128,161 +204,65 @@ def main(cfg):
         the opened global config dictionairy, passed by ESMValTool.
 
     """
-    #print(cfg)
-    settings_file = cfg['input_files']
     metadatas = diagtools.get_input_files(cfg)
-    metadata_files = metadatas.keys()
-
-
-
-    model_metadatas = {}
-    for fn in metadata_files:
-        print('\nmetadata_file:', fn)
-        print(metadatas[fn])
-
-        if metadatas[fn]['dataset'] == 'CORA':
-            continue # Obs
-        if metadatas[fn]['start_year'] == metadatas[fn]['end_year'] == 1950:
-            model_metadatas['1950'] = fn
-        if metadatas[fn]['start_year'] == metadatas[fn]['end_year'] == 2000:
-            model_metadatas['2000'] = fn
-
-        if metadatas[fn]['start_year'] == metadatas[fn]['end_year'] == 2050:
-            model_metadatas['2050'] = fn
-        if metadatas[fn]['start_year'] == metadatas[fn]['end_year'] == 2090:
-            model_metadatas['2099'] = fn
-
-        if metadatas[fn]['start_year'] == 1950 and metadatas[fn]['end_year'] == 1955:
-            model_metadatas['1950-1955'] = fn
-
-        if metadatas[fn]['start_year'] == 2085 and metadatas[fn]['end_year'] == 2089:
-            model_metadatas['2095-2099'] = fn
 
     # Load image format extention
     image_extention = diagtools.get_image_format(cfg)
 
-    print(model_metadatas)
-    model_cubes = {}
-    for name, model_fn in model_metadatas.items():
-        cube = iris.load_cube(model_fn)
-        model_cubes[name] = cube
+    model_cubes = prepare_model_metadata(cfg)
 
-    thresholds =[33., 33.5, 34., 34.5, 35., 35.5]
+
+    thresholds =[33., 33.5, 34., 34.5, 35., 35.5, 36., 36.5, 37.]
+    levels =[33., 34., 35., 36., 37.]
     linestyles = ['-' for thres in thresholds]
     colours = ['k' for thres in thresholds]
-    linewidths = [1., 0.5, 1., 0.5, 1., 0.5,]
+    linewidths = [1., 0.5, 1., 0.5, 1., 0.5, 1., 0.5, 1.,]
 
-
-
+    thresholds_white = np.arange(-2, 2.25, 0.25)
+    change_ticks = [-0.2, -0.1, 0., 0.1, 0.2]
+    linestyles_white = ['-' for thres in thresholds_white]
+    colours_white = ['w' for thres in thresholds_white]
+    linewidths_white = [0.5 for thres in thresholds_white]
 
     fig = plt.figure()
     fig.set_size_inches(10, 6)
 
     # Create the cubes
-    # cube221 = cubes['model'][layer]
-    # cube222 = cubes['obs'][layer]
+    cube221 = load_cube_221()
+    cube222 = load_cube_222()
+    cube221_contours = cube222
+    cube222_contours = cube222
+    cube221_contours_white = cube221 # load_cube_trends()
+    cube222_contours_white = cube221
+
     cube223 = model_cubes['2000'] - model_cubes['1950']
-    cube223_contours = model_cubes['1950-1955']
     cube224 = model_cubes['2099'] - model_cubes['2050']
-    cube224_contours = model_cubes['2095-2099']
-
-
-    # create the z axis for plots 2, 3, 4.
-    # zrange12 = diagtools.get_cube_range([cube221, cube222])
-    # cube224.data = np.ma.clip(cube224.data, 0.1, 10.)
+    cube223_contours = model_cubes['1950-2000']
+    cube224_contours = model_cubes['2050-2099']
+    cube223_contours_white = cube223
+    cube224_contours_white = cube224
 
     n_points = 9
-    # linspace12 = np.linspace(
-    #     zrange12[0], zrange12[1], n_points, endpoint=True)
+    lims = [34., 37.]
+    linspace_sal = np.linspace(lims[0], lims[1], n_points*3, endpoint=True)
     lims = [-0.2, 0.2]
-    linspace_all = np.linspace(lims[0], lims[1], n_points, endpoint=True)
-    print(linspace_all)
-    #assert 0
+    linspace_change = np.linspace(lims[0], lims[1], n_points, endpoint=True)
 
-    # Add the sub plots to the figure.
-    # add_map_subplot(221, cube221, linspace12, cmap='viridis', title=model)
-    # add_map_subplot(
-    #     222, cube222, linspace12, cmap='viridis', title=' '.join([
-    #         obs,
-    #     ]))
-
-
-    # add_map_subplot(
-    #     223,
-    #     cube223,
-    #     linspace_all,
-    #     cmap='bwr',
-    #     title='',
-    #     cbar = False)
-    sp221 = plt.subplot(221)
+    #############################################
+    # 221
+    sp221 = plt.subplot(221, projection=ccrs.PlateCarree(central_longitude=180.0))
+    pyplot.title('A: Obs. change')
     qplot = iris.plot.contourf(
-        cube223,
-        linspace_all,
+        cube221,
+        linspace_change,
         linewidth=0,
-        cmap=plt.cm.get_cmap('RdYlBu'),
-        zmin=linspace_all.min(),
-        zmax=linspace_all.max(),
-        rasterized=True)
-    plt.gca().add_feature(cartopy.feature.LAND, facecolor=[0.8, 0.8, 0.8])
-    plt.gca().coastlines()
-    cbar = pyplot.colorbar(orientation='horizontal')
-
-    sp222 = plt.subplot(222)
-    qplot = iris.plot.contourf(
-        cube223,
-        linspace_all,
-        linewidth=0,
-        cmap=plt.cm.get_cmap('RdYlBu'),
-        zmin=linspace_all.min(),
-        zmax=linspace_all.max(),
-        rasterized=True)
-    plt.gca().add_feature(cartopy.feature.LAND, facecolor=[0.8, 0.8, 0.8])
-    plt.gca().coastlines()
-    cbar = pyplot.colorbar(orientation='horizontal')
-
-    print(linspace_all)
-    sp223 = plt.subplot(223)
-    qplot = iris.plot.contourf(
-        cube223,
-        linspace_all,
-        linewidth=0,
-        cmap=plt.cm.get_cmap('RdYlBu'),
-        zmin=linspace_all.min(),
-        zmax=linspace_all.max(),
-        rasterized=True,
-        extend='neither',
-        )
-    print('cube223:',cube223.data.min(), cube223.data.max())
-    print('cube224:',cube224.data.min(), cube224.data.max())
-
-    # TODO: problem here is that the contourf plot just ignores the linspace and zmin/zmax ranges.
-
-    cbar = pyplot.colorbar(orientation='horizontal')
-
-    iris.plot.contour(cube223_contours,
-                 thresholds,
-                 colors=colours,
-                 linewidths=linewidths,
-                 linestyles=linestyles,
-                 rasterized=True,
-                 extend='both',
-                 )
-    plt.gca().add_feature(cartopy.feature.LAND, facecolor=[0.8, 0.8, 0.8])
-    plt.gca().coastlines()
-
-    sp224 = plt.subplot(224)
-    mesh41 = qplot = iris.plot.contourf(
-        cube224,
-        linspace_all,
-        linewidth=0,
-        cmap=plt.cm.get_cmap('RdYlBu'),
-        zmin=linspace_all.min(),
-        zmax=linspace_all.max(),
+        cmap=plt.cm.get_cmap('RdYlBu_r'),
         extend='both',
         rasterized=True)
-    cbar = pyplot.colorbar(orientation='horizontal')
+    add_coasts()
+    cbar = pyplot.colorbar(orientation='horizontal', ticks=change_ticks)
 
-    mesh42 = iris.plot.contour(cube224_contours,
+    c221 = iris.plot.contour(cube221_contours,
                  thresholds,
                  colors=colours,
                  linewidths=linewidths,
@@ -290,29 +270,126 @@ def main(cfg):
                  rasterized=True,
                  extend='both',
                  )
-    plt.gca().add_feature(cartopy.feature.LAND, facecolor=[0.8, 0.8, 0.8])
-    plt.gca().coastlines()
+    sp221.clabel(c221, levels, inline=True, fontsize=8, fmt = '%1.0f')
 
-    # fig.suptitle(long_name, fontsize=14)
-    # Add overall title
+    c221w = iris.plot.contour(cube221_contours_white,
+                 thresholds_white,
+                 colors=colours_white,
+                 linewidths=linewidths_white,
+                 linestyles=linestyles_white,
+                 rasterized=True,
+                 extend='both',
+                 )
 
-    # fig.subplots_adjust(hspace=0.01, wspace=0.01)
-    #
-    # fig.subplots_adjust(right=0.9)
-    # cb_ax = fig.add_axes([0.93, 0.02, 0.02, 0.9])
-    # cbar = fig.colorbar(mesh41, cax=cb_ax)
-    # plt.colorbar() #img, cax=cbar_ax)
+    #############################################
+    # 222
+    sp222 = plt.subplot(222, projection=ccrs.PlateCarree(central_longitude=180.0))
+    pyplot.title('B: Obs. mean')
+    qplot = iris.plot.contourf(
+        cube222,
+        linspace_sal,
+        linewidth=0,
+        cmap=plt.cm.get_cmap('RdYlBu_r'),
+        extend='both',
+        rasterized=True)
+    cbar = pyplot.colorbar(orientation='horizontal', ticks=levels)
+    add_coasts()
 
+    c222 = iris.plot.contour(cube222_contours,
+                 thresholds,
+                 colors=colours,
+                 linewidths=linewidths,
+                 linestyles=linestyles,
+                 rasterized=True,
+                 extend='both',
+                 )
+    sp222.clabel(c222, levels, inline=True, fontsize=8, fmt = '%1.0f')
+    # c222w = iris.plot.contour(cube222_contours_white,
+    #              thresholds_white,
+    #              colors=colours_white,
+    #              linewidths=linewidths_white,
+    #              linestyles=linestyles_white,
+    #              rasterized=True,
+    #              extend='both',
+    #              )
+
+    #############################################
+    # 223
+    sp223 = plt.subplot(223, projection=ccrs.PlateCarree(central_longitude=180.0))
+    pyplot.title('C: CMIP5 hist.')
+    qplot = iris.plot.contourf(
+        cube223,
+        linspace_change,
+        linewidth=0,
+        cmap=plt.cm.get_cmap('RdYlBu_r'),
+        rasterized=True,
+        extend='both',
+        )
+    cbar = pyplot.colorbar(orientation='horizontal', ticks=change_ticks)
+    add_coasts()
+
+    c223 = iris.plot.contour(cube223_contours,
+                 thresholds,
+                 colors=colours,
+                 linewidths=linewidths,
+                 linestyles=linestyles,
+                 rasterized=True,
+                 extend='both',
+                 )
+    sp223.clabel(c223, levels, inline=True, fontsize=8, fmt = '%1.0f')
+    c223w = iris.plot.contour(cube223_contours_white,
+                 thresholds_white,
+                 colors=colours_white,
+                 linewidths=linewidths_white,
+                 linestyles=linestyles_white,
+                 rasterized=True,
+                 extend='both',
+                 )
+
+    #############################################
+    # 224
+    sp224 = plt.subplot(224, projection=ccrs.PlateCarree(central_longitude=180.0))
+    pyplot.title('D: CMIP5 RCPs')
+
+    mesh41 = qplot = iris.plot.contourf(
+        cube224,
+        linspace_change,
+        linewidth=0,
+        cmap=plt.cm.get_cmap('RdYlBu_r'),
+        extend='both',
+        rasterized=True)
+    cbar = pyplot.colorbar(orientation='horizontal', ticks=change_ticks)
+    add_coasts()
+    c224 = iris.plot.contour(cube224_contours,
+                 thresholds,
+                 colors=colours,
+                 linewidths=linewidths,
+                 linestyles=linestyles,
+                 rasterized=True,
+                 extend='both',
+                 )
+
+    sp224.clabel(c224, levels, inline=True, fontsize=8, fmt = '%1.0f')
+    c224w = iris.plot.contour(cube224_contours_white,
+                 thresholds_white,
+                 colors=colours_white,
+                 linewidths=linewidths_white,
+                 linestyles=linestyles_white,
+                 rasterized=True,
+                 extend='both',
+                 )
+
+    #############################################
     # Determine image filename:
     path = diagtools.folder(cfg['plot_dir']) + 'fig_3_21' + image_extention
 
     # Saving files:
     if cfg['write_plots']:
         logger.info('Saving plots to %s', path)
-        plt.savefig(path , )#bbox_inches = 'tight',)
+        fig.savefig(path , )#bbox_inches = 'tight',)
+        fig.savefig(path.replace('.pdf', '.png') , dpi=250 )
 
     plt.close()
-
 
     logger.info('Success')
 
