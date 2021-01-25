68ca12b9dc509f272f7e7afd76fa2023692a09d9
“katjaweigel” <weigel@iup.physik.uni-bremen.de>
2021-01-25 15:01:24 +0100

    Prospector messages reduced.

--- esmvaltool/diag_scripts/santer20jclim/santer20jclimfig.py
+++ esmvaltool/diag_scripts/santer20jclim/santer20jclimfig.py
@@ -48,28 +48,9 @@ def _apply_filter(cfg, cube):
     """Apply filter from RSS Anomalies to all data and calculates mean."""
     if 'filter' in cfg:
         filt = iris.load(cfg['filter'])[0]
-        timefil = filt.coord('time')
-        latfil = filt.coord('latitude')
-        lonfil = filt.coord('longitude')
-        timecu = cube.coord('time')
 
-        start = timecu.units.num2date(timecu.points[0])
-        end = timecu.units.num2date(timecu.points[-1])
-        stime = timefil.nearest_neighbour_index(timefil.units.date2num(start))
-        etime = timefil.nearest_neighbour_index(timefil.units.date2num(end))
-        filt = filt[stime:etime + 1, :, :]
-
-        startlat = cube.coord('latitude').points[0]
-        endlat = cube.coord('latitude').points[-1]
-        slat = latfil.nearest_neighbour_index(startlat)
-        elat = latfil.nearest_neighbour_index(endlat)
-        filt = filt[:, slat:elat + 1, :]
-
-        startlon = cube.coord('longitude').points[0]
-        endlon = cube.coord('longitude').points[-1]
-        slon = lonfil.nearest_neighbour_index(startlon)
-        elon = lonfil.nearest_neighbour_index(endlon)
-        filt = filt[:, :, slon:elon + 1]
+        for iii, dim_str in ['time', 'latitude', 'longitude']:
+            filt = _fit_dim(filt, cube, dim_str, iii)
 
         cube.data = cube.data * filt.data
 
@@ -79,6 +60,40 @@ def _apply_filter(cfg, cube):
                            weights=cube_grid_areas))
 
     return cube
+
+
+def _fit_dim(filt, cube, dim_str, dim_nr):
+    """Adjust array for given dimension."""
+    dimfil = filt.coord(dim_str)
+    dimcu = cube.coord(dim_str)
+    if dim_str == 'time':
+        start = dimcu.units.num2date(dimcu.points[0])
+        end = dimcu.units.num2date(dimcu.points[-1])
+        sdim = dimfil.nearest_neighbour_index(dimfil.units.date2num(start))
+        edim = dimfil.nearest_neighbour_index(dimfil.units.date2num(end))
+    else:
+        start = dimcu.points[0]
+        end = dimcu.points[-1]
+        sdim = dimfil.nearest_neighbour_index(start)
+        edim = dimfil.nearest_neighbour_index(end)
+
+    if dim_nr == 0:
+        filt = filt[sdim:edim + 1, :, :]
+    elif dim_nr == 1:
+        filt = filt[:, sdim:edim + 1, :]
+    elif dim_nr == 2:
+        filt = filt[:, :, sdim:edim + 1]
+
+    return filt
+
+
+def _calc_trend(cube_anom):
+    """Calculate trends."""
+    reg_var = stats.linregress(np.linspace(0, len(cube_anom.data) - 1,
+                                           len(cube_anom.data)),
+                               cube_anom.data)
+
+    return reg_var.slope
 
 
 def _calculate_anomalies(cube):
@@ -120,142 +135,69 @@ def _check_full_data(dataset_path, cube):
     return check
 
 
-def _get_sel_files(cfg, dataname, dim=2):
-    """Get filenames from cfg for single models or multi-model mean."""
-    selection = []
-    if dim == 2:
-        for hlp in select_metadata(cfg['input_data'].values(),
-                                   dataset=dataname):
-            selection.append(hlp['filename'])
-    else:
-        for hlp in cfg['input_data'].values():
-            selection.append(hlp['filename'])
-
-    return selection
-
-
-def _get_sel_files_var(cfg, varnames):
-    """Get filenames from cfg for all model mean and differen variables."""
-    selection = []
-
-    for var in varnames:
-        for hlp in select_metadata(cfg['input_data'].values(), short_name=var):
-            selection.append(hlp['filename'])
-
-    return selection
-
-
 def _plot_extratrends(cfg, extratrends, trends):
     """Plot trends for ensembles."""
     xhist = np.linspace(0, 4, 41)
     artrend = {}
     kde1 = {}
     fig, axx = plt.subplots(figsize=(8, 6))
-    maxh = 2.5
 
+    valid_datasets = []
+    
     for extramodel in cfg['add_model_dist']:
-        teststr = list(extratrends[extramodel].keys())[0]
-        if teststr in trends['cmip6'].keys():
+        alias = list(extratrends[extramodel].keys())[0]
+        valid_datasets.append(select_metadata(cfg['input_data'].values(),
+                                              alias=alias)[0]['filename'])
+        if alias in trends['cmip6'].keys():
             style = plot.get_dataset_style(extramodel, style_file='cmip6')
-            facec = style['facecolor']
-            edgec = style['color']
-            colc = style['color']
-        elif teststr in trends['cmip5'].keys():
+        elif alias in trends['cmip5'].keys():
             style = plot.get_dataset_style(extramodel, style_file='cmip5')
-            facec = style['facecolor']
-            edgec = style['color']
-            colc = style['color']
         else:
-            facec = (0, 0, 1, 0.1)
-            edgec = (0, 0, 1, 0.6)
-            colc = (0, 0, 1, 1.0)
+            style = {'facecolor': (0, 0, 1, 0.1), 'color': (0, 0, 1, 1.0)}
 
         artrend[extramodel] = np.fromiter(extratrends[extramodel].values(),
                                           dtype=float)
         kde1[extramodel] = stats.gaussian_kde(artrend[extramodel],
                                               bw_method="scott")
         axx.hist(artrend[extramodel], bins=xhist, density=True,
-                 edgecolor=edgec,
-                 facecolor=facec)
+                 edgecolor=style['color'],
+                 facecolor=style['facecolor'])
         axx.plot(xhist, kde1[extramodel](xhist),
-                 color=colc,
+                 color=style['color'],
                  linewidth=3,
                  label=extramodel)
 
-    if trends['obs']:
-        for iii, obsname in enumerate(trends['obs'].keys()):
-            obscoli = float(iii)
-            if iii > 4:
-                obscoli = float(iii) - 4.5
-            if iii > 8:
-                obscoli = float(iii) - 8.75
-            if iii > 12:
-                obscoli = float(iii) - 12.25
-            axx.vlines(trends['obs'][obsname], 0, maxh,
-                       colors=(1.0 - 0.25 * obscoli, 0.25 * obscoli,
-                               0.5 + obscoli * 0.1),
-                       linewidth=3,
-                       label=obsname)
-
-    axx.legend(loc=0)
-    axx.set_ylim([0, 2.5])
-    axx.set_ylabel('Probability density')
-    axx.set_xlabel('Trend in Water Vapor Path [%/dec]')
-    fig.tight_layout()
-    fig.savefig(get_plot_filename('fig2', cfg), dpi=300)
-
+    obs_str = _plot_obs(trends, axx, 2.5)
+    _plot_settings(cfg, axx, fig, 'fig2')
     plt.close()
 
+    caption = 'Probability density function of the decadal trend in ' + \
+        'the Water Vapor Path.' + obs_str
 
-def _plot_trends(cfg, trends, valid_datasets):
-    """Plot probability density function of trends."""
-    xhist = np.linspace(0, 4, 41)
-    # CMIP5
-    if trends['cmip5']:
-        artrend_c5 = np.fromiter(trends['cmip5'].values(), dtype=float)
-        weights_c5 = np.fromiter(trends['cmip5weights'].values(), dtype=float)
-        # yyy_c5, xxx_c5 = np.histogram(artrend_c5, bins=xhist)
-        kde1_c5 = stats.gaussian_kde(artrend_c5, weights=weights_c5,
-                                     bw_method="scott")
-    # CMIP6
-    if trends['cmip6']:
-        artrend_c6 = np.fromiter(trends['cmip6'].values(), dtype=float)
-        weights_c6 = np.fromiter(trends['cmip6weights'].values(), dtype=float)
-        # yyy_c6, xxx_c6 = np.histogram(artrend_c6, bins=xhist)
-        kde1_c6 = stats.gaussian_kde(artrend_c6, weights=weights_c6,
-                                     bw_method="scott")
+    provenance_record = get_provenance_record(valid_datasets,
+                                              caption, ['trend', 'other'],
+                                              ['reg'])
 
-    fig, axx = plt.subplots(figsize=(8, 6))
+    diagnostic_file = get_diagnostic_filename('fig2', cfg)
 
-    maxh = 2.5
-    # CMIP5
-    if trends['cmip5']:
-        axx.hist(artrend_c5, bins=xhist, density=True,
-                 weights=weights_c5,
-                 edgecolor=(0, 0, 1, 0.6),
-                 facecolor=(0, 0, 1, 0.1))
-        axx.plot(xhist, kde1_c5(xhist),
-                 color=(0, 0, 1, 1),
-                 linewidth=3,
-                 label="CMIP5")
-        # maxh = np.max(kde1_c5(xhist))
+    logger.info("Saving analysis results to %s", diagnostic_file)
 
-    # CMIP6
-    if trends['cmip6']:
-        axx.hist(artrend_c6, bins=xhist, density=True,
-                 weights=weights_c6,
-                 edgecolor=(0.8, 0.4, 0.1, 0.6),
-                 facecolor=(0.8, 0.4, 0.1, 0.1))
-        axx.plot(xhist, kde1_c6(xhist),
-                 color=(0.8, 0.4, 0.1, 1),
-                 linewidth=3,
-                 label="CMIP6")
-        # maxh = np.max(kde1_c6(xhist))
+    list_dict = _write_list_dict(cfg, trends, xhist, artrend, kde1)
+
+    iris.save(cube_to_save_vars(list_dict), target=diagnostic_file)
+
+    logger.info("Recording provenance of %s:\n%s", diagnostic_file,
+                pformat(provenance_record))
+    with ProvenanceLogger(cfg) as provenance_logger:
+        provenance_logger.log(diagnostic_file, provenance_record)
+
+
+def _plot_obs(trends, axx, maxh):
+    """Plot observational or reanalysis data as vertical line."""
     obs_str = ''
     if trends['obs']:
         obs_str = ' Vertical lines show the trend for'
         for iii, obsname in enumerate(trends['obs'].keys()):
-            obs_str = obs_str + ' ' + obsname
             obscoli = float(iii)
             if iii > 4:
                 obscoli = float(iii) - 4.5
@@ -269,14 +211,49 @@ def _plot_trends(cfg, trends, valid_datasets):
                        linewidth=3,
                        label=obsname)
         obs_str = obs_str + '.'
+    return obs_str
 
-    axx.legend(loc=0)
-    axx.set_ylim([0, 2.5])
-    axx.set_ylabel('Probability density')
-    axx.set_xlabel('Trend in Water Vapor Path [%/dec]')
-    fig.tight_layout()
-    fig.savefig(get_plot_filename('fig1', cfg), dpi=300)
 
+def _plot_trends(cfg, trends, valid_datasets):
+    """Plot probability density function of trends."""
+    xhist = np.linspace(0, 4, 41)
+    fig, axx = plt.subplots(figsize=(8, 6))
+
+    # CMIP5
+    if trends['cmip5']:
+        artrend_c5 = np.fromiter(trends['cmip5'].values(), dtype=float)
+        weights_c5 = np.fromiter(trends['cmip5weights'].values(), dtype=float)
+        # yyy_c5, xxx_c5 = np.histogram(artrend_c5, bins=xhist)
+        kde1_c5 = stats.gaussian_kde(artrend_c5, weights=weights_c5,
+                                     bw_method="scott")
+        axx.hist(artrend_c5, bins=xhist, density=True,
+                 weights=weights_c5,
+                 edgecolor=(0, 0, 1, 0.6),
+                 facecolor=(0, 0, 1, 0.1))
+        axx.plot(xhist, kde1_c5(xhist),
+                 color=(0, 0, 1, 1),
+                 linewidth=3,
+                 label="CMIP5")
+        # maxh = np.max(kde1_c5(xhist))
+
+    # CMIP6
+    if trends['cmip6']:
+        artrend_c6 = np.fromiter(trends['cmip6'].values(), dtype=float)
+        weights_c6 = np.fromiter(trends['cmip6weights'].values(), dtype=float)
+        # yyy_c6, xxx_c6 = np.histogram(artrend_c6, bins=xhist)
+        kde1_c6 = stats.gaussian_kde(artrend_c6, weights=weights_c6,
+                                     bw_method="scott")
+        axx.hist(artrend_c6, bins=xhist, density=True,
+                 weights=weights_c6,
+                 edgecolor=(0.8, 0.4, 0.1, 0.6),
+                 facecolor=(0.8, 0.4, 0.1, 0.1))
+        axx.plot(xhist, kde1_c6(xhist),
+                 color=(0.8, 0.4, 0.1, 1),
+                 linewidth=3,
+                 label="CMIP6")
+        # maxh = np.max(kde1_c6(xhist))
+    obs_str = _plot_obs(trends, axx, 2.5)
+    _plot_settings(cfg, axx, fig, 'fig1')
     plt.close()
 
     caption = 'Probability density function of the decadal trend in ' + \
@@ -340,6 +317,47 @@ def _plot_trends(cfg, trends, valid_datasets):
         provenance_logger.log(diagnostic_file, provenance_record)
 
 
+def _plot_settings(cfg, axx, fig, figname):
+    """Define Settings for pdf figures."""
+    axx.legend(loc=0)
+    axx.set_ylim([0, 2.5])
+    axx.set_ylabel('Probability density')
+    axx.set_xlabel('Trend in Water Vapor Path [%/dec]')
+    fig.tight_layout()
+    fig.savefig(get_plot_filename(figname, cfg), dpi=300)
+
+
+def _write_list_dict(cfg, trends, xhist, artrend, kde1):
+    """Collect data for provenance."""
+    list_dict = {}
+    list_dict["data"] = [xhist]
+    list_dict["name"] = [{'var_name': 'prw_trends_bins',
+                          'long_name': 'Water Vapor Path Trend bins',
+                          'units': 'percent'}]
+
+    for extramodel in cfg['add_model_dist']:
+        list_dict["data"].append(artrend[extramodel])
+        list_dict["name"].append({'var_name': 'prw_trends_' + extramodel,
+                                  'long_name': 'Water Vapor Path Trends ' +
+                                               extramodel,
+                                  'units': 'percent'})
+        list_dict["data"].append(kde1[extramodel](xhist))
+        list_dict["name"].append({'var_name': 'prw_trend_distribution_' +
+                                              extramodel,
+                                  'long_name': 'Water Vapor Path Trends ' +
+                                               'distribution ' + extramodel,
+                                  'units': '1'})
+
+    if trends['obs']:
+        for obsname in trends['obs'].keys():
+            list_dict["data"].append(trends['obs'][obsname])
+            list_dict["name"].append({'var_name': 'prw_trend_' + obsname,
+                                      'long_name': 'Water Vapor Path Trend ' +
+                                                   obsname,
+                                      'units': 'percent'})
+    return list_dict
+
+
 def cube_to_save_vars(list_dict):
     """Create cubes to prepare bar plot data for saving to netCDF."""
     # cubes = iris.cube.CubeList()
@@ -381,51 +399,9 @@ def get_provenance_record(ancestor_files, caption, statistics,
     return record
 
 
-def plot_ts_and_trend(cfg, cube_anom, dataset):
-    """Plot time series and calculate trends."""
-    reg_var = stats.linregress(np.linspace(0, len(cube_anom.data) - 1,
-                                           len(cube_anom.data)),
-                               cube_anom.data)
-    y_reg = reg_var.slope * np.linspace(0, len(cube_anom.data) - 1,
-                                        len(cube_anom.data)) + \
-        reg_var.intercept
-
-    fig, axx = plt.subplots(figsize=(8, 6))
-    axx.plot(np.linspace(0, len(cube_anom.data) - 1, len(cube_anom.data)),
-             y_reg, color='k',
-             label='trend = ' + str(reg_var.slope) + ' %/dec')
-    axx.plot(np.linspace(0, len(cube_anom.data) - 1, len(cube_anom.data)),
-             cube_anom.data,
-             color='b',
-             marker='x')
-
-    axx.legend(loc=0, framealpha=1)
-    fig.tight_layout()
-
-    fig.savefig(get_plot_filename('ts_' + dataset, cfg), dpi=300)
-
-    plt.close()
-
-    return reg_var.slope
-
-
 ###############################################################################
 # Setup diagnostic
 ###############################################################################
-
-
-def get_reg_2d_li(mism_diff_rain, ar_hist_rain, lats, lons):
-    """Linear regression of 1D and 2D array, returns 2D array of p and r."""
-    reg2d = np.zeros((len(lats), len(lons), 4))
-    for iii in range(len(lats)):
-        for jjj in range(len(lons)):
-            reg = stats.linregress(mism_diff_rain, ar_hist_rain[iii, jjj, :])
-            reg2d[iii, jjj, 0] = reg.rvalue
-            reg2d[iii, jjj, 1] = reg.pvalue
-            reg2d[iii, jjj, 2] = reg.slope
-            reg2d[iii, jjj, 3] = reg.intercept
-
-    return reg2d
 
 
 def main(cfg):
@@ -451,10 +427,12 @@ def main(cfg):
     trends['cmip5weights'] = OrderedDict()
     trends['cmip6weights'] = OrderedDict()
     trends['obs'] = {}
-    f_c5 = open(cfg['work_dir'] + "cmip5_trends.txt", "a")
-    f_c5.write("Model Alias Trend Weight Mean Median Maximum Mnimum Standard deviation \n")
-    f_c6 = open(cfg['work_dir'] + "cmip6_trends.txt", "a")
-    f_c6.write("Model Alias Trend Weight Mean Median Maximum Mnimum Standard deviation \n")
+    f_c5 = open(cfg['work_dir'] + "/cmip5_trends.txt", "a")
+    f_c5.write("Model Alias Trend Weight Mean Median " +
+               "Maximum Mnimum Standard deviation \n")
+    f_c6 = open(cfg['work_dir'] + "/cmip6_trends.txt", "a")
+    f_c6.write("Model Alias Trend Weight Mean Median " +
+               "Maximum Mnimum Standard deviation \n")
 
     if 'add_model_dist' in cfg:
         extratrends = {}
@@ -476,7 +454,7 @@ def main(cfg):
                 number_of_subdata[dataset] = number_of_subdata[dataset] - 1
             else:
                 valid_datasets.append(dataset_path['filename'])
-                
+
     for dataset_path in input_data:
         project = dataset_path['project']
         cube_load = iris.load(dataset_path['filename'])[0]
@@ -492,10 +470,8 @@ def main(cfg):
             continue
         cube_anom = _calculate_anomalies(cube)
 
-        
-
         if project == 'CMIP6':
-            trend = plot_ts_and_trend(cfg, cube_anom, alias)
+            trend = _calc_trend(cube_anom)
             trends['cmip6'][alias] = trend
             trends['cmip6weights'][alias] = 1. / number_of_subdata[dataset]
             print(dataset, alias, trend, 1. / number_of_subdata[dataset])
@@ -509,7 +485,7 @@ def main(cfg):
                        str(round(np.min(cube_anom.data), 4)) + ' ' +
                        str(round(np.std(cube_anom.data), 4)) + '\n')
         elif project == 'CMIP5':
-            trend = plot_ts_and_trend(cfg, cube_anom, alias)
+            trend = _calc_trend(cube_anom)
             trends['cmip5'][alias] = trend
             trends['cmip5weights'][alias] = 1. / number_of_subdata[dataset]
             f_c5.write(dataset + ' ' +
@@ -522,7 +498,7 @@ def main(cfg):
                        str(round(np.min(cube_anom.data), 4)) + ' ' +
                        str(round(np.std(cube_anom.data), 4)) + '\n')
         else:
-            trend = plot_ts_and_trend(cfg, cube_anom, dataset)
+            trend = _calc_trend(cube_anom)
             trends['obs'][dataset] = trend
 
         if 'add_model_dist' in cfg:
